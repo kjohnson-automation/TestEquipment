5bcd8955b7924a750f7066ab9ceb67271f66b3f5
kmj442 <kmj442@gmail.com>
2019-10-03 19:40:42 -0400

    updated readme initial VisaHandler.py commit, barely defines base class.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # TestEquipment
 Library of test equipment handlers.
+
+Language: Python 3.7
+
+This library will be used to control and number of pieces of equipment.
+
+Current supported library with explicit functionality:
+- In development
---
+++ VisaHandler.py
@@ -0,0 +1,11 @@
+# This is the base controller for VISA based devices.
+# From here all subClasses of VISA devices will inhereit base functionality to increase productivity and add features
+
+import os, sys, pyvisa
+
+class Visa_Device():
+    """ Base class for VISA device communications """
+
+    def __init__(self):
+        """ Creates the initial connection and checks settings before proceeding """
+        pass
